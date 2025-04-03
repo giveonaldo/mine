4ccd88aaceb478306db2a9e03ab51ac63299cbac
giveonaldo <abriansyahadam5@gmail.com>
2025-04-04 02:07:45 +0700

    update text in stories card

--- src/components/StoriesCard.jsx
+++ src/components/StoriesCard.jsx
@@ -4,7 +4,7 @@ function StoriesCard({image, desc, link}) {
   return (
     <a href={link} className='text-wrap flex w-full h-auto border gap-4 border-black items-center transition duration-300 ease-out p-4 md:hover:shadow-custome md:shadow-none shadow-custome '>
       <img className='w-14 h-14 object-cover flex-none ' src={image} alt='me and my girlfriend' />
-      <p className='text-xs font-serif line-clamp-3'>{desc}</p>
+      <p className='md:text-xs text-[10px] font-thin  font-serif line-clamp-3'>{desc}</p>
     </a>
   )
 }
